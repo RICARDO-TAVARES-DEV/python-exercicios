1ab50311e6c1738354f0402e496b895ae1b058c4
Ricardo <ricotavares7719@gmail.com>
2025-11-21 09:19:14 -0300

    Acho que ta indo

--- exercicios2.py
+++ exercicios2.py
@@ -1,77 +1,77 @@
-#------------------MANIPULAÇÃO DE STRINGS----------------------------
-# EXERCICIO 1 PEDIR AO USUARIO UMA PALAVRA E ESTA APARECER INVERTIDA
-palavra = input("Digite uma palavra: ")
-#  transforma a string em lista de caracteres
-# #Peguei a minha palavra e transformei em uma lista pra depois inverter com o reverse
-lista = list(palavra) 
-print("Lista de caracteres:", lista)
-# inverte a lista aqui eu utilizei o reverse pra inverter a minha lista 
-lista.reverse()
-print("Lista invertida:", lista)
-# junta de volta em uma string e aqui eu ja estou usando o join pra voltar a ser uma string
-novapalavra = "".join(lista)
-print("Palavra invertida:", novapalavra)
-#-----------------------------------------------------------------------------------------------------
+# #------------------MANIPULAÇÃO DE STRINGS----------------------------
+# # EXERCICIO 1 PEDIR AO USUARIO UMA PALAVRA E ESTA APARECER INVERTIDA
+# palavra = input("Digite uma palavra: ")
+# #  transforma a string em lista de caracteres
+# # #Peguei a minha palavra e transformei em uma lista pra depois inverter com o reverse
+# lista = list(palavra) 
+# print("Lista de caracteres:", lista)
+# # inverte a lista aqui eu utilizei o reverse pra inverter a minha lista 
+# lista.reverse()
+# print("Lista invertida:", lista)
+# # junta de volta em uma string e aqui eu ja estou usando o join pra voltar a ser uma string
+# novapalavra = "".join(lista)
+# print("Palavra invertida:", novapalavra)
+# #-----------------------------------------------------------------------------------------------------
 
-#EXERCICIO 2:CONTAGEM DE VOGAIS EM UMA PALAVRA DIGITADA PELO USUARIO
-palavra = input("Digite uma palavra: ")
-vogais = ["a", "e", "i", "o", "u"]
-contador = 0 #o contador vai servir pra contar quantas vogais estão contidas na palavra
-for letra in palavra: # O laço de repetição for vai verificar cada letra na palavra
-    if letra in vogais:# caso encontre vogal some um e no final descobrimos quantas vogais temos
-        contador += 1
+# #EXERCICIO 2:CONTAGEM DE VOGAIS EM UMA PALAVRA DIGITADA PELO USUARIO
+# palavra = input("Digite uma palavra: ")
+# vogais = ["a", "e", "i", "o", "u"]
+# contador = 0 #o contador vai servir pra contar quantas vogais estão contidas na palavra
+# for letra in palavra: # O laço de repetição for vai verificar cada letra na palavra
+#     if letra in vogais:# caso encontre vogal some um e no final descobrimos quantas vogais temos
+#         contador += 1
 
-print(f"A palavra '{palavra}' tem {contador} vogais.")
+# print(f"A palavra '{palavra}' tem {contador} vogais.")
 
-#EXERCICIO 3 : PALINDROMO
-palavra1 = input("Digite sua primeira palavra: ")
-palavra2 = input("Digite a sua segunda palavra: ")
+# #EXERCICIO 3 : PALINDROMO
+# palavra1 = input("Digite sua primeira palavra: ")
+# palavra2 = input("Digite a sua segunda palavra: ")
 
-# Converter a primeira palavra em uma lista.
-lista1 = list(palavra1)
+# # Converter a primeira palavra em uma lista.
+# lista1 = list(palavra1)
 
-# Converter a segunda palavra em outra lista e inverter
-lista2 = list(palavra2)
-lista2.reverse()
+# # Converter a segunda palavra em outra lista e inverter
+# lista2 = list(palavra2)
+# lista2.reverse()
 
-# Verificar se as duas são iguais
-if lista1 == lista2:
-    print("Isso é um palíndromo")
-else:
-    print("Isso não é um palíndromo")
+# # Verificar se as duas são iguais
+# if lista1 == lista2:
+#     print("Isso é um palíndromo")
+# else:
+#     print("Isso não é um palíndromo")
     
-#Exercicio 4: Substituição de palavras
-frase=input("Digite sua frase com pelo menos três palavras:")
-listadepalavras=frase.split()
-if len(listadepalavras) >=3:
-    listadepalavras[3]="Amora"
-    novafrase =" ".join(listadepalavras)
-    print("Nova frase :" ,novafrase)
-else:
-    print("A frase precisa de pelo menos 3 palavras")
+# #Exercicio 4: Substituição de palavras
+# frase=input("Digite sua frase com pelo menos três palavras:")
+# listadepalavras=frase.split()
+# if len(listadepalavras) >=3:
+#     listadepalavras[3]="Amora"
+#     novafrase =" ".join(listadepalavras)
+#     print("Nova frase :" ,novafrase)
+# else:
+#     print("A frase precisa de pelo menos 3 palavras")
     
-#Exercicio 5: Nome com sobrenome = nome completo
-nome=input("Digite seu nome : ")
-sobrenome=input("Digite seu sobrenome: ")
-nomecompleto=(nome+" "+sobrenome)
-print(nomecompleto)
+# #Exercicio 5: Nome com sobrenome = nome completo
+# nome=input("Digite seu nome : ")
+# sobrenome=input("Digite seu sobrenome: ")
+# nomecompleto=(nome+" "+sobrenome)
+# print(nomecompleto)
 
-Exercicio 6:Verificar se são anagramas
-palavra1 = input('Digite sua primeira palavra: ')
-palavra2 = input('Digite a outra palavra: ')
+# Exercicio 6:Verificar se são anagramas
+# palavra1 = input('Digite sua primeira palavra: ')
+# palavra2 = input('Digite a outra palavra: ')
 
-Converter em listas de letras ordenadas
-novalista1 = sorted(palavra1)
-novalista2 = sorted(palavra2)
+# Converter em listas de letras ordenadas
+# novalista1 = sorted(palavra1)
+# novalista2 = sorted(palavra2)
 
-print(novalista1)
-print(novalista2)
+# print(novalista1)
+# print(novalista2)
 
-Comparando as listas ordenadas iremos verificar se são anagramas ou não.
-if novalista1 == novalista2:
-    print("São anagramas")
-else:
-    print("Não são anagramas")
+# Comparando as listas ordenadas iremos verificar se são anagramas ou não.
+# if novalista1 == novalista2:
+#     print("São anagramas")
+# else:
+#     print("Não são anagramas")
 
        
 
--- exercicios3.py
+++ exercicios3.py
@@ -1,6 +1,81 @@
-f = open('arquivo_de_trabalho', 'r', encoding="utf-8")
-with open('arquivo_de_trabalho', encoding="utf-8") as f:
-    read_data = f.read()
+#a=append(incrementar)
+#r=read(leitura)
+#w=write(escrita)
+#r+=leitura + escrita
+# palavra=open("teste2.txt","x")
+# print(palavra)
+# print(palavra.readable())
+# print(palavra.read())
+# print(palavra.readline())
+# print(palavra.readline())
+# print(palavra.readline())
+# print(palavra.readline())
 
-# Podemos verificar se o arquivo foi fechado automaticamente.
-f.closed
+# lista = palavra.readlines()
+# print(lista)
+
+# print(lista[4])
+
+# palavra.write("\nPython")
+# palavra.write("\nJava Script")
+# palavra.write("\nPython")
+
+# palavra.close()
+
+# import os
+# if os.path.exists("teste1.txt"):
+#      os.remove("teste1.txt")
+# else:
+#         print("Arquivo não existe")
+
+# # import os
+# # os.rmdir("./nova pasta")
+
+#****************Manipulação de arquivos ***************************
+#Exercicio 1 Leitura de uma arquivo de texto e exibir na tela
+# texto=open("teste.txt","a")
+# print("texto.txt")
+# texto.close()
+#-----------------------------------------------------------------
+#Exercicio 2 Peça ao usuario pra inserir um texto e salvar em um arquivo
+# arquivo = input(">> Digite o nome do arquivo para ler: ")
+# with open(arquivo, "r") as x:
+#     leitor = x.read()
+#     print("## Conteúdo do arquivo:\n" + leitor)
+#---------------------------------------------------------------------------------       
+#Exercicio 3 Contagem de linhas conte o numero de linhas em um aarquivo de texto
+# arquivo = input(">> Digite o nome do arquivo para contar linhas: ")
+
+# with open(arquivo, "r") as teste:
+#     linhas = teste.readlines()
+#     linha_conta = len(linhas)
+#     print("## Número de linhas no arquivo: " + str(linha_conta))         
+#----------------------------------------------------------------------------------------
+
+#Exercicio 4 Cópia de um arquivo crie a cópia de um arquivo existente
+# arquivo1 = input(">> Digite o nome do arquivo de origem: ")
+# arquivo2 = input(">> Digite o nome do arquivo de destino: ")
+
+# with open(arquivo1, "r") as f1:
+#     content = f1.read()
+#     with open(arquivo2, "w") as f2:
+#         f2.write(content)
+
+#------------------------------------------------------------------------------------------
+
+#Exercicio 5 Adicionar conteudo em um arquivo. Peça ao usuario adicionar um texto no final de um arquivo existente
+
+# arquivo = input(">> Digite o nome do arquivo para adicionar conteúdo: ")
+# with open(arquivo, "a") as f1:
+#     content = input(">> Digite o conteúdo para adicionar ao arquivo: ")
+#     f1.write(content + "\n")
+#     print("## Conteúdo adicionado ao arquivo com sucesso.")
+
+
+
+
+
+
+
+
+
